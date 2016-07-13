5a64964be9b11d78b15dfea4d58ffd6f1dd09683
Safort <a.asafort@gmail.com>
2016-07-13 21:41:56 +0300

    added echo bot functionality

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "gopkg.in/telegram-bot-api.v4"
     "./config"
 )
 
 func main() {
     cnfg := config.Load()
 
-    fmt.Println("BotToken =", cnfg.BotToken)
-    fmt.Println("OwmToken =", cnfg.OwmToken)
+    bot, err := tgbotapi.NewBotAPI(cnfg.BotToken)
+
+    if err != nil {
+        log.Panic(err)
+    }
+
+    log.Printf("Authorized on account %s", bot.Self.UserName)
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates, err := bot.GetUpdatesChan(u)
+
+    for update := range updates {
+        if update.Message == nil {
+            continue
+        }
+
+        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+        bot.Send(msg)
+    }
 }
