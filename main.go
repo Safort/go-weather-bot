8289cf9eec9291ad48ce7f6e246f434725d2cf93
Safort <a.asafort@gmail.com>
2016-07-14 03:14:01 +0300

    change echo bot: now user getting JSON with data about requested city

--- main.go
+++ main.go
@@ -2,9 +2,39 @@ package main
 
 import (
     "log"
+    "net/http"
+    "io/ioutil"
     "gopkg.in/telegram-bot-api.v4"
     "./config"
 )
+
+
+func Answer(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+    cnfg := config.Load()
+
+    log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+    url := "http://api.openweathermap.org/data/2.5/forecast/daily?"
+    url += "APPID=" + cnfg.OwmToken + "&"
+    url += "q=" + update.Message.Text + "&"
+    url += "lang=ru"
+
+    res, err := http.Get(url)
+    if err != nil {
+        log.Fatal(err)
+    }
+    json, err := ioutil.ReadAll(res.Body)
+    res.Body.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    msg := tgbotapi.NewMessage(update.Message.Chat.ID, string(json))
+
+    bot.Send(msg)
+}
+
+
 
 func main() {
     cnfg := config.Load()
@@ -23,14 +53,11 @@ func main() {
     updates, err := bot.GetUpdatesChan(u)
 
     for update := range updates {
+        
         if update.Message == nil {
             continue
         }
 
-        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-        msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-
-        bot.Send(msg)
+        go Answer(bot, update)
     }
 }
