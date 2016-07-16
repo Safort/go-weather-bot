848289254afa24c7409bd5bd1ceb455484720d3e
Safort <a.asafort@gmail.com>
2016-07-16 23:03:10 +0300

    added weathermap

---
+++ weathermap/weathermap.go
@@ -0,0 +1,41 @@
+package weathermap
+
+
+type Coord struct {
+    Lon, Lat float64
+}
+
+type Weather struct {
+    Id int
+    Main string
+    Description string
+    Icon string
+}
+
+type City struct {
+    Id int
+    Name string
+    Coord Coord
+    Country string
+}
+
+type Temp struct {
+    Day, Min, Max, Night, Eve, Morn float64
+}
+
+type ListItem struct {
+    Dt int
+    Temp Temp
+    Pressure float64
+    Humidity int
+    Weather []Weather
+    Speed float64
+    Deg float64
+    Clouds int
+}
+
+type WeatherMap struct {
+    City City
+    Cod string
+    List []ListItem
+}
