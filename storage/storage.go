fa7beac69c85cc18749db17d9ce5e6f914501d00
Safort <a.asafort@gmail.com>
2016-07-18 23:40:37 +0300

    updated storage

--- storage/storage.go
+++ storage/storage.go
@@ -1,27 +1,34 @@
 package storage
 
 import (
+    "time"
+    "strconv"
     "../weathermap"
 )
 
 
 type StorageItem struct {
     weathermap.WeatherMap
-    lastUpdate int
+    LastUpdate int
 }
 
 type Storage struct {
     cities map[string]StorageItem
 }
 
-func (self *Storage) Set(name string, info weathermap.WeatherMap) {
-    self.cities[name] = StorageItem{info, 10}
-}
+func (self *Storage) Set(name string, info weathermap.WeatherMap) StorageItem {
+    t := time.Now()
+    LastUpdate, _ := strconv.Atoi(t.Format("20060102150405"))
+    self.cities[name] = StorageItem{info, LastUpdate}
 
-func (self *Storage) Get(name string) StorageItem {
     return self.cities[name]
 }
 
+func (self *Storage) Get(name string) (StorageItem, bool) {
+    item, ok := self.cities[name]
+    
+    return item, ok
+}
 
 func NewStorage() *Storage {
     return &Storage{cities: make(map[string]StorageItem)}
